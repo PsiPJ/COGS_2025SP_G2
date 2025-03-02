a148358992f6c1812d4495e0fd214bb4c0d831c8
jyatch <Jakemoulds05@gmail.com>
2025-03-02 18:00:53 -0500

    added mobs to game scene and hit box
    Made it so mobs constantly spawn from a spawn point on the left.
    
    There is also a hitbox and temporary health bar for collision testing

---
+++ game_scene.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func spawn_mob():
+	var new_mob = preload("res://mob.tscn").instantiate()
+	new_mob.global_position = $"%SpawnPoint".global_position
+	add_child(new_mob)
+
+
+func _on_timer_timeout() -> void:
+	spawn_mob()

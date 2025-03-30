d7947e9e5ef0194c6193e679ddb843f3b38dd0f2
jyatch <Jakemoulds05@gmail.com>
2025-03-30 16:54:11 -0400

    made animation for menu scene

--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ var health = 100.0
 #controls for player and speed
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left_%s" % [player_id], "move_right_%s" % [player_id], "move_up_%s" % [player_id], "move_down_%s" % [player_id])
-	velocity = direction * 6000 * delta #delta might be unecessary
+	velocity = direction * 60000 * delta #delta might be unecessary
 	move_and_slide()
 
 	const DAMAGE_RATE = 5.0

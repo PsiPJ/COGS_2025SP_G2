9387ac45809a4d6d56386b3da4e2309c312e8d37
jyatch <Jakemoulds05@gmail.com>
2025-04-03 17:56:34 -0400

    *finished* the minigame manager, with arbitrary scores and a timer

--- main.gd
+++ main.gd
@@ -6,11 +6,39 @@ extends Node2D
 # MiniGameManager node in our main scene.
 @export var mini_game_manager: MiniGameManager
 
+# when the game starts, for each player_data we will load our player scene,
+# set its properties according to the save data, and spawn them in.
+# We specifically set their player_id, color, and positions here
 func _on_node_game_started(player_data_array: Array) -> void:
 	for player_data in player_data_array:
-		print(player_data.number)
 		var player = preload("res://player.tscn").instantiate()
 		player.player_id = player_data.number
 		player.global_position = Vector2(350 + (100 * player_data.number), 220)
+		player.get_node("Sprite2D").modulate = Color(player_data.color)
 		add_child(player)
-		print("Spawning player")
+
+# length of game is currently determined by this "time_left" variable. We can change
+# this is the future to be something else, or base the game on something other than time
+# such as wait for everyone to die.
+var time_left = 1
+func _process(delta: float):
+	time_left -= delta
+	if time_left < 0:
+		# End the game after the time is up
+		end_game()
+		
+func end_game():
+	# Here, we can add some code that distributes points based on the amount of
+	# health each player has left. For now, the points are just arbitrarily given
+	# for test purposes
+	
+	mini_game_manager.end_game([
+		{
+			"player": 0,
+			"points": 5
+		},
+		{
+			"player": 1,
+			"points": 5
+		}
+	])

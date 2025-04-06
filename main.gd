584ee728acdf80cb71e6002dda80275443ee5bbc
jyatch <Jakemoulds05@gmail.com>
2025-04-06 17:01:49 -0400

    made the players spawn after the menu from timer

--- main.gd
+++ main.gd
@@ -6,21 +6,18 @@ extends Node2D
 # MiniGameManager node in our main scene.
 @export var mini_game_manager: MiniGameManager
 
+var new_player_data
+
 # when the game starts, for each player_data we will load our player scene,
 # set its properties according to the save data, and spawn them in.
 # We specifically set their player_id, color, and positions here
 func _on_node_game_started(player_data_array: Array) -> void:
-	for player_data in player_data_array:
-		var player = preload("res://player.tscn").instantiate()
-		player.player_id = player_data.number
-		player.global_position = Vector2(350 + (100 * player_data.number), 220)
-		player.get_node("Sprite2D").modulate = Color(player_data.color)
-		add_child(player)
+	new_player_data = player_data_array
 
 # length of game is currently determined by this "time_left" variable. We can change
 # this is the future to be something else, or base the game on something other than time
 # such as wait for everyone to die.
-var time_left = 1
+var time_left = 10
 func _process(delta: float):
 	time_left -= delta
 	if time_left < 0:
@@ -42,3 +39,12 @@ func end_game():
 			"points": 5
 		}
 	])
+
+
+func _on_timer_timeout() -> void:
+	for player_data in new_player_data:
+		var player = preload("res://player.tscn").instantiate()
+		player.player_id = player_data.number
+		player.global_position = Vector2(350 + (100 * player_data.number), 220)
+		player.get_node("Sprite2D").modulate = Color(player_data.color)
+		add_child(player)

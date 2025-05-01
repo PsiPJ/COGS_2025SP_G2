07b498f0ecb86eec6bcfab74b2d623d26a78fba3
jyatch <Jakemoulds05@gmail.com>
2025-05-01 13:13:06 -0400

    health bar implemented
    (still need to add invincibility frames)

--- player.gd
+++ player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 @export var player_id = 1 # Used to tell which player this is (for multiplayer)
 
-var health = 100.0 # Starting health
+var health = 3 # Starting health
+
+var ded_texture = preload("res://Ded.png")
 
 func _ready(): 
 	add_to_group("player") # Add this node to the "player" group
@@ -17,22 +19,15 @@ func _physics_process(delta):
 		"move_down_%s" % [player_id]
 	)
 	
-	velocity = direction * 60000 * delta # Multiplies direction by speed and delta time
+	velocity = direction * 30000 * delta # Multiplies direction by speed and delta time
 	move_and_slide() # Built-in Godot movement function
 
-	# Health draining logic (commented out for now)
-	# const DAMAGE_RATE = 5.0  
-	# var overlapping_mobs = %HrtBox.get_overlapping_bodies()
-	# if overlapping_mobs.size() > 0:
-	#     health -= DAMAGE_RATE * overlapping_mobs.size() * delta # Lose more health the more mobs you're touching
-	#     %ProgressBar.value = health # Update the health bar on screen
-
-# This function is called when the player takes damage
-func ouch(id):
-	Global.lose_life() # Tell the global script to reduce lives
-	set_modulate(Color(1, 0.3, 0.3, 0.3)) # Flash red to show damage taken
-	$RedTimer.start() # Start a timer that will reset the color after a bit
-
-# Called when RedTimer finishes
-func _on_red_timer_timeout() -> void:
-	set_modulate(Color(1, 1, 1, 1)) # Return to normal color
+# this function will execute everytime an enemy enters the players hitbox
+# upon getting hit, the player will take damage, and if health goes to 0 we queue free
+func _on_hurt_box_area_entered(area: Area2D) -> void:
+	if health > 0:
+		var heart_node = $HealthBar.get_node("Heart%d" % health)
+		heart_node.texture = ded_texture
+		health -= 1
+		if health == 0:
+			queue_free()
